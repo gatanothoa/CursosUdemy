013de3b778368302dec0e6e1d566b608bedfd7ef
Antonio Córdova Carmona <antonio.cordova.c@gmail.com>
2025-11-17 14:50:10 -0600

    Auto: actualizar resúmenes 2025-11-17

--- Python/Día cuatro/operadoresDeCompatación.py
+++ Python/Día cuatro/operadoresDeCompatación.py
@@ -1,3 +1,13 @@
-mi_bool = 10 == 25 
+num1 = 36
+num2 = 17   
 
-print(mi_bool)
+mi_bool = num1 >= num2
+
+print("El resultado de la comparación es:", mi_bool)
+
+num3 = 25 ** 0.5
+num4 = 5
+
+mi_bool1 = num3 == num4
+print("El resultado de la comparación es:", mi_bool)
+
